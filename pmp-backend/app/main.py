9e1c6c07082a2e69f5f2d53fcd39c245c1606a40
Rafayasad <asadrafay998@gmail.com>
2025-11-11 17:17:15 +0500

    Enhance CORS configuration: Add local development origin to allow requests from specific IP address

--- pmp-backend/app/main.py
+++ pmp-backend/app/main.py
@@ -188,6 +188,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://api.rento.online",
+        "http://192.168.8.48:8000",
         "https://admin.rento.online",
         "https://rento.online",
         "http://localhost:3005",
